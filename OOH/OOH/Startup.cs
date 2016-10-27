b784b81adeeadaf18d3206c03442a585a05277ff
rangeles324 <idrisdaiz@gmail.com>
2016-10-26 21:32:13 -0400

    initial setup

---
+++ OOH/OOH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OOH.Startup))]
+namespace OOH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
